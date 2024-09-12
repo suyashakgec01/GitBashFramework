6914cae824ea5e0b837b29f85768913df5ec485a
suyashakgec01 <suyashakgec01@gmail.com>
2024-09-12 20:36:05 +0530

    first commit

---
+++ src/test/java/com/basicselenium/Sample.java
@@ -0,0 +1,10 @@
+package com.basicselenium;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Sample {
+	public static void main(String[] args) {
+		ChromeDriver cd1 = new ChromeDriver();
+	}
+
+}
